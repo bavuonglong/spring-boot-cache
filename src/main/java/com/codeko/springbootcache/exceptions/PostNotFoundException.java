61a9879972bcc1001f2844e0c7d82333aa8ecad4
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-08-15 18:29:58 +0700

    Done

---
+++ src/main/java/com/codeko/springbootcache/exceptions/PostNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codeko.springbootcache.exceptions;
+
+public class PostNotFoundException extends Exception {
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
